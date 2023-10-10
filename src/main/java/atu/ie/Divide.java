9da29c53802fd7c8b969a73adfd140931ec0f850
Igor Blok <G00415662@atu.ie>
2023-10-10 13:38:35 +0100

    Added file Divide, Calculation of Divison

---
+++ src/main/java/atu/ie/Divide.java
@@ -0,0 +1,29 @@
+package atu.ie;
+
+import java.util.Scanner;
+
+public class Divide {
+
+    public static void main(String[] args){
+        add();
+
+    }
+
+    public static void add()
+    {
+        System.out.println("Please enter your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int FirstNum = inputs.nextInt();
+
+        System.out.println("Please enter your Second Number :");
+        int SecondNum = inputs.nextInt();
+
+        int total = FirstNum / SecondNum;
+        System.out.println("The Total for Divison is " + total);
+    }
+
+    public static void delete()
+    {
+
+    }
+}
