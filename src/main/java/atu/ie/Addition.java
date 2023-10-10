dca3ab73dc609ea6ff1a7b1f658b7fa4a65c958d
Igor Blok <G00415662@atu.ie>
2023-10-10 14:01:16 +0100

    Added Addition, Calculator to add

---
+++ src/main/java/atu/ie/Addition.java
@@ -0,0 +1,28 @@
+package atu.ie;
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args){
+        add();
+
+    }
+
+    public static void add()
+    {
+        System.out.println("Please enter your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int FirstNum = inputs.nextInt();
+
+        System.out.println("Please enter your Second Number :");
+        int SecondNum = inputs.nextInt();
+
+        int total = FirstNum + SecondNum;
+        System.out.println("The Total for multiplication is " + total);
+    }
+
+    public static void delete()
+    {
+
+    }
+}
